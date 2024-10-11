91b0f2518f31f335c8d62a09910b6d3398286625
Ennyajax123 <ajanaku.eniola123@gmail.com>
2024-10-11 10:03:51 +0100

    updated frontend,backend &Databsase-install.tf

--- Backend-install.sh
+++ Backend-install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo yum install git -y
 sudo yum install python -y
-sudo yum install netcat -y
--- frontend-install.sh
+++ frontend-install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo yum install git -y
 sudo yum install npm -y 
-sudo yum install netcat -y
