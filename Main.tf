30d83ef874ad32bb0b93bb2a5cf8167ec4f5044d
Ennyajax123 <ajanaku.eniola123@gmail.com>
2024-10-11 09:47:43 +0100

    updated Main.tf, output.tf and variable files.

--- Main.tf
+++ Main.tf
@@ -2,26 +2,21 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">4.16"
-      required_version  =">1.2.0"
+      version = "5.70.0"
     }
   }
 }
-
-# Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
 }
-
 # Create a VPC
 resource "aws_vpc" "example1" {
   cidr_block = "10.0.0.0/16"
 }
-
 # creating the frontend system
 
-resource "aws_security_group" "frontend-sg" {
-  name = "MProj1-frontend-sg"
+resource "aws_security_group" "frontend-1-sg" {
+  name = "MProj1-frontend-1-sg"
   # ... other configuration ...
 
   egress {
@@ -48,22 +43,63 @@ resource "aws_security_group" "frontend-sg" {
   }
 }
 
-resource "aws_instance" "front-end" {
-  ami                    = var.frontend_ami
-  instance_type          = var.frontend_instance_type
-  key_name               = var.frontend_key_name
+resource "aws_instance" "frontend-1" {
+  ami                    = var.frontend-1_ami
+  instance_type          = var.frontend-1_instance_type
+  availability_zone      = "eu-west-2b"
+  key_name               = var.frontend-1_key_name
   user_data              = file("./frontend-install.sh")
-  vpc_security_group_ids = [aws_security_group.frontend-sg.id]
+  vpc_security_group_ids = [aws_security_group.frontend-1-sg.id]
   tags = {
-    Name = "front-end"
+    Name = "frontend-1"
   }
 }
 
+# creating the frontend system
 
+resource "aws_security_group" "frontend-2-sg" {
+  name = "MProj1-frontend-2-sg"
+  # ... other configuration ...
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_instance" "frontend-2" {
+  ami               = var.frontend-2_ami
+  instance_type     = var.frontend-2_instance_type
+  availability_zone = "eu-west-2b"
+  key_name          = var.frontend-2_key_name
+  user_data              = file("./frontend-install.sh")
+  vpc_security_group_ids = [aws_security_group.frontend-2-sg.id]
+  tags = {
+    Name = "frontend-2"
+  }
+}
 
 # creating the backend system
-resource "aws_security_group" "backend-sg" {
-  name = "MProj1-backend-sg"
+resource "aws_security_group" "backend-1-sg" {
+  name = "MProj1-backend-1-sg"
   # ... other configuration ...
 
   egress {
@@ -90,22 +126,62 @@ resource "aws_security_group" "backend-sg" {
   }
 }
 
-resource "aws_instance" "backend" {
-  ami                    = var.backend_ami
-  instance_type          = var.backend_instance_type
-  key_name               = var.backend_key_name
-  #user_data              = file("./backend-install.sh")
-  vpc_security_group_ids = [aws_security_group.backend-sg.id]
+resource "aws_instance" "backend-1" {
+  ami               = var.backend-1_ami
+  instance_type     = var.backend-1_instance_type
+  availability_zone = "eu-west-2a"
+  key_name          = var.backend-1_key_name
+  user_data              = file("./backend-install.sh")
+  vpc_security_group_ids = [aws_security_group.backend-1-sg.id]
   tags = {
-    Name = "backend"
+    Name = "backend-1"
+  }
+}
+
+# creating the backend system
+resource "aws_security_group" "backend-2-sg" {
+  name = "MProj1-backend-2-sg"
+  # ... other configuration ...
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+resource "aws_instance" "backend-2" {
+  ami               = var.backend-2_ami
+  instance_type     = var.backend-2_instance_type
+  availability_zone = "eu-west-2b"
+  key_name          = var.backend-2_key_name
+  user_data              = file("./backend-install.sh")
+  vpc_security_group_ids = [aws_security_group.backend-2-sg.id]
+  tags = {
+    Name = "backend-2"
   }
 }
 
 
-
 # creating the Database system
-resource "aws_security_group" "Database-sg" {
-  name = "MProj1-Database-sg"
+resource "aws_security_group" "database-1-sg" {
+  name = "MProj1-Database-1-sg"
   # ... other configuration ...
 
   egress {
@@ -132,30 +208,69 @@ resource "aws_security_group" "Database-sg" {
   }
 }
 
-resource "aws_instance" "Database" {
-  ami                    = var.Database_ami
-  instance_type          = var.Database_instance_type
-  key_name               = var.Database_key_name
-  #user_data              = file("./Database-install.sh")
-  vpc_security_group_ids = [aws_security_group.Database-sg.id]
+resource "aws_instance" "database-1" {
+  ami               = var.database-1_ami
+  instance_type     = var.database-1_instance_type
+  availability_zone = "eu-west-2a"
+  key_name          = var.database-1_key_name
+  user_data              = file("./Database-install.sh")
+  vpc_security_group_ids = [aws_security_group.database-1-sg.id]
   tags = {
-    Name = "Database"
+    Name = "Database-1"
   }
 }
 
+# creating the Database system
+resource "aws_security_group" "database-2-sg" {
+  name = "MProj1-Database-2-sg"
+  # ... other configuration ...
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_instance" "database-2" {
+  ami               = var.database-2_ami
+  instance_type     = var.database-2_instance_type
+  availability_zone = "eu-west-2b"
+  key_name          = var.database-2_key_name
+  user_data              = file("./Database-install.sh")
+  vpc_security_group_ids = [aws_security_group.database-2-sg.id]
+  tags = {
+    Name = "Database-2"
+  }
+}
 variable "region_number1" {
   # Arbitrary mapping of region name to number to use in
   # a VPC's CIDR prefix.
   default = {
-    eu-east-1      = 1
-    eu-west-2      = 2
-    eu-west-3      = 3
+    eu-west-1 = 1
+    eu-west-2 = 2
+    eu-west-3 = 3
   }
 }
 
-variable "AD_number1" {
+variable "AZ_number1" {
   # Assign a number to each AZ letter used in our configuration
   default = {
     a = 1
@@ -164,19 +279,5 @@ variable "AD_number1" {
   }
 }
 
-# Retrieve the AZ where we want to create network resources
-# This must be in the region selected on the AWS provider.
-data "aws_availability_zone" "example" {
-  name = "eu-west-2a"
-  }
 
-# Create a VPC for the region associated with the AZ
-resource "aws_vpc" "example" {
-  cidr_block = cidrsubnet ("10.0.0.0/16", 4, var.region_number[data.aws_availability_zone.example.region])
-}
 
-# Create a subnet for the AZ within the regional VPC
-resource "aws_subnet" "example" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = cidrsubnet(aws_vpc.example.cidr_block, 4, var.AD_number[data.aws_availability_zone.example.name_suffix])
-}
--- output.tf
+++ output.tf
@@ -1,11 +1,23 @@
-output "front-end_public_dns" {
-  value = aws_instance.front-end.public_dns
+output "frontend-1_public_dns" {
+  value = aws_instance.frontend-1.public_dns
 }
 
-output "backend_public_dns" {
-  value = aws_instance.backend.public_dns
+output "backend-1_public_dns" {
+  value = aws_instance.backend-1.public_dns
 }
 
-output "Databaseend_public_dns" {
-  value = aws_instance.backend.public_dns
+output "database-1_public_dns" {
+  value = aws_instance.database-1.public_dns
+}
+
+output "frontend-2_public_dns" {
+  value = aws_instance.frontend-2.public_dns
+}
+
+output "backend-2_public_dns" {
+  value = aws_instance.backend-2.public_dns
+}
+
+output "database-2_public_dns" {
+  value = aws_instance.database-2.public_dns
 }
